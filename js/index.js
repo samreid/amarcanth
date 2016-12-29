91c09771c0d6c1b994ee15eca1953d34db8a8c92
samreid <reids@colorado.edu>
2016-12-28 21:35:12 -0700

    Added geometry from example

--- js/index.js
+++ js/index.js
@@ -35,18 +35,15 @@
   var scene = new THREE.Scene();
 
   var camera = new THREE.PerspectiveCamera( 90, 1, 0.001, 700 );
-  camera.position.set( 0, 10, 0 );
+  camera.position.set( -10, 10, 0 );
   scene.add( camera );
 
   var controls = new THREE.OrbitControls( camera, element );
-  controls.rotateUp( Math.PI / 4 );
   controls.target.set(
     camera.position.x + 0.1,
     camera.position.y,
     camera.position.z
   );
-  controls.noZoom = true;
-  controls.noPan = true;
 
   function setOrientationControls( e ) {
     if ( !e.alpha ) {
@@ -63,9 +60,6 @@
   }
 
   window.addEventListener( 'deviceorientation', setOrientationControls, true );
-
-  var light = new THREE.HemisphereLight( 0x777777, 0x000000, 0.6 );
-  scene.add( light );
 
   var texture = THREE.ImageUtils.loadTexture(
     'textures/patterns/checker.png'
@@ -96,6 +90,36 @@
   window.addEventListener( 'resize', resize, false );
   setTimeout( resize, 1 );
 
+  var loader = new THREE.AssimpLoader();
+  var octaminatorAnimation = null;
+  loader.load( '../three.js/examples/models/assimp/octaminator/Octaminator.assimp', function( err, result ) {
+
+    var object = result.object;
+
+    object.position.x = 30;
+    object.position.y = 13;
+    object.rotation.x = 0;
+    object.rotation.y = Math.PI / 2 + Math.PI / 12;
+    var scale = 0.05;
+    object.scale.x = scale;
+    object.scale.y = scale;
+    object.scale.z = scale;
+    scene.add( object );
+
+    octaminatorAnimation = result.animation;
+  } );
+
+  var axisHelper = new THREE.AxisHelper( 5 );
+  scene.add( axisHelper );
+
+  var ambient = new THREE.HemisphereLight( 0x8888fff, 0xff8888, 0.5 );
+  ambient.position.set( 0, 1, 0 );
+  scene.add( ambient );
+
+  var light = new THREE.DirectionalLight( 0xffffff, 1 );
+  light.position.set( 0, 4, 4 ).normalize();
+  scene.add( light );
+
   animate();
 
   function resize() {
@@ -120,6 +144,8 @@
       sphere.position.x = Math.sin( Date.now() / 1000 ) * 200 + 300;
       sphere.position.z = Math.cos( Date.now() / 2730 ) * 200 + 100;
     }
+
+    octaminatorAnimation && octaminatorAnimation.setTime( performance.now() / 1000 );
   }
 
   function render( dt ) {
