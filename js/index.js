9931416e67f83e9cc103be635b9e6baef2b23694
samreid <reids@colorado.edu>
2016-12-28 19:44:09 -0700

    simplified init

--- js/index.js
+++ js/index.js
@@ -1,101 +1,94 @@
 (function() {
 
-  var camera, scene, renderer;
-  var effect, controls;
-  var element, container;
+  var clock = new THREE.Clock();
   var sphere = null;
 
-  var clock = new THREE.Clock();
+  var renderer = new THREE.WebGLRenderer();
+  var element = renderer.domElement;
+  var container = document.getElementById( 'example' );
+  container.appendChild( element );
 
-  init();
-  animate();
+  var pointerDown = function( event ) {
+    console.log( 'hello' );
+    event.preventDefault();
 
-  function init() {
-    renderer = new THREE.WebGLRenderer();
-    element = renderer.domElement;
-    container = document.getElementById( 'example' );
-    container.appendChild( element );
+    sphere = new THREE.Mesh( new THREE.SphereGeometry( 30 ), sphereMaterial );
+    sphere.position.x = 100 + 20;
+    sphere.position.y = 40;
 
-    var pointerDown = function( event ) {
-      console.log( 'hello' );
-      event.preventDefault();
+    scene.add( sphere );
+  };
+  element.addEventListener( 'mousedown', pointerDown, false );
+  element.addEventListener( 'touchstart', pointerDown, false );
 
-      sphere = new THREE.Mesh( new THREE.SphereGeometry( 30 ), sphereMaterial );
-      sphere.position.x = 100 + 20;
-      sphere.position.y = 40;
+  var effect = new THREE.StereoEffect( renderer );
 
-      scene.add( sphere );
-    };
-    element.addEventListener( 'mousedown', pointerDown, false );
-    element.addEventListener( 'touchstart', pointerDown, false );
+  var scene = new THREE.Scene();
 
-    effect = new THREE.StereoEffect( renderer );
+  var camera = new THREE.PerspectiveCamera( 90, 1, 0.001, 700 );
+  camera.position.set( 0, 10, 0 );
+  scene.add( camera );
 
-    scene = new THREE.Scene();
+  var controls = new THREE.OrbitControls( camera, element );
+  controls.rotateUp( Math.PI / 4 );
+  controls.target.set(
+    camera.position.x + 0.1,
+    camera.position.y,
+    camera.position.z
+  );
+  controls.noZoom = true;
+  controls.noPan = true;
 
-    camera = new THREE.PerspectiveCamera( 90, 1, 0.001, 700 );
-    camera.position.set( 0, 10, 0 );
-    scene.add( camera );
-
-    controls = new THREE.OrbitControls( camera, element );
-    controls.rotateUp( Math.PI / 4 );
-    controls.target.set(
-      camera.position.x + 0.1,
-      camera.position.y,
-      camera.position.z
-    );
-    controls.noZoom = true;
-    controls.noPan = true;
-
-    function setOrientationControls( e ) {
-      if ( !e.alpha ) {
-        return;
-      }
-
-      controls = new THREE.DeviceOrientationControls( camera, true );
-      controls.connect();
-      controls.update();
-
-      element.addEventListener( 'click', fullscreen, false );
-
-      window.removeEventListener( 'deviceorientation', setOrientationControls, true );
+  function setOrientationControls( e ) {
+    if ( !e.alpha ) {
+      return;
     }
 
-    window.addEventListener( 'deviceorientation', setOrientationControls, true );
+    var controls = new THREE.DeviceOrientationControls( camera, true );
+    controls.connect();
+    controls.update();
 
+    element.addEventListener( 'click', fullscreen, false );
 
-    var light = new THREE.HemisphereLight( 0x777777, 0x000000, 0.6 );
-    scene.add( light );
-
-    var texture = THREE.ImageUtils.loadTexture(
-      'textures/patterns/checker.png'
-    );
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat = new THREE.Vector2( 50, 50 );
-    texture.anisotropy = renderer.getMaxAnisotropy();
-
-    var material = new THREE.MeshPhongMaterial( {
-      color: 0xffffff,
-      specular: 0xffffff,
-      shininess: 20,
-      shading: THREE.FlatShading,
-      map: texture
-    } );
-
-    var geometry = new THREE.PlaneGeometry( 1000, 1000 );
-
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.rotation.x = -Math.PI / 2;
-    scene.add( mesh );
-
-    const sphereMaterial = new THREE.MeshLambertMaterial( {
-      color: 0xCC00FF
-    } );
-
-    window.addEventListener( 'resize', resize, false );
-    setTimeout( resize, 1 );
+    window.removeEventListener( 'deviceorientation', setOrientationControls, true );
   }
+
+  window.addEventListener( 'deviceorientation', setOrientationControls, true );
+
+
+  var light = new THREE.HemisphereLight( 0x777777, 0x000000, 0.6 );
+  scene.add( light );
+
+  var texture = THREE.ImageUtils.loadTexture(
+    'textures/patterns/checker.png'
+  );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat = new THREE.Vector2( 50, 50 );
+  texture.anisotropy = renderer.getMaxAnisotropy();
+
+  var material = new THREE.MeshPhongMaterial( {
+    color: 0xffffff,
+    specular: 0xffffff,
+    shininess: 20,
+    shading: THREE.FlatShading,
+    map: texture
+  } );
+
+  var geometry = new THREE.PlaneGeometry( 1000, 1000 );
+
+  var mesh = new THREE.Mesh( geometry, material );
+  mesh.rotation.x = -Math.PI / 2;
+  scene.add( mesh );
+
+  const sphereMaterial = new THREE.MeshLambertMaterial( {
+    color: 0xCC00FF
+  } );
+
+  window.addEventListener( 'resize', resize, false );
+  setTimeout( resize, 1 );
+
+  animate();
 
   function resize() {
     var width = container.offsetWidth;
